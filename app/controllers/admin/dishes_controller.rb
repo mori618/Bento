ccf7441ef4a3a6a07e9e2320fdb4f007ad7fe943
mori618 <148190402+mori618@users.noreply.github.com>
2024-01-04 07:18:28 +0000

    森　おかず編集

--- app/controllers/admin/dishes_controller.rb
+++ app/controllers/admin/dishes_controller.rb
@@ -18,17 +18,34 @@ class Admin::DishesController < ApplicationController
     if @dish.save
       redirect_to admin_dish_path(@dish), notice: "作成を完了しました"
     else
-      puts @dish.errors.full_messages
       render :new
     end
   end
 
+  def destroy
+    @dish =Dish.find(params[:id])
+    @dish.destroy
+    redirect_to admin_dishes_path
+  end
+
   def edit
+    @dish = Dish.find(params[:id])
+    @genres = GenreDish.all
+  end
+
+  def update
+    @dish = Dish.find(params[:id])
+    @genres = GenreDish.all
+    if @dish.update(dish_params)
+      redirect_to admin_dish_path(@dish), notice: "変更内容を保存しました。"
+    else
+      render :edit
+    end
   end
 
   private
 
   def dish_params
-    params.require(:dish).permit(:name, :introduction, :image, :genre_id,:is_active)
+    params.require(:dish).permit(:name, :introduction, :image, :genre_id,:is_active,:dish_image)
   end
 end
